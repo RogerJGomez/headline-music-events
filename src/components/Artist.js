f55e7750d071f7ea163b28e1b6f598b4e931a2c2
Roger Gómez <gomez.roger779@gmail.com>
2020-06-20 14:58:46 -0400

    Fixed style issues

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -3,6 +3,10 @@ import Event from "./Event";
 import { Spinner, Grid, Cell } from "react-mdl";
 import Border from "./Border";
 
+const mainStyles = {
+  background: "#7118d7", color: "white"
+}
+
 export default function Artist({ events, artist, loading, error }) {
   if (loading) {
     return (
@@ -16,7 +20,7 @@ export default function Artist({ events, artist, loading, error }) {
   } else if (error) {
     return (
       <>
-        <Grid style={{ background: "#7118d7", color: "white" }}>
+        <Grid style={mainStyles}>
           <Cell col={12}>
             <div id="title">
               <h3 style={{ color: "white" }}>Artist not found</h3>
@@ -29,7 +33,7 @@ export default function Artist({ events, artist, loading, error }) {
   } else if (events.length > 0) {
     return (
       <>
-        <Grid style={{ background: "#7118d7", color: "white" }}>
+        <Grid style={mainStyles}>
           <Cell col={12}>
             <div id="title">
               <img draggable="false" src={artist.image_url} alt="artist" />
@@ -40,31 +44,28 @@ export default function Artist({ events, artist, loading, error }) {
           </Cell>
         </Grid>
         <Border />
-        <Grid
-          style={{ margin: "0 5vw 5vw", padding: "2%", textAlign: "center" }}
-        >
+        <Grid style={{margin:"0 5%"}}>
           {events.map((event) => {
             return <Event key={event.id} event={event} />;
           })}
         </Grid>
       </>
     );
-  } else {
-    return (
-      <>
-        <Grid style={{ background: "#7118d7", color: "white" }}>
-          <Cell col={12}>
-            <div id="title">
-              <h3 style={{ color: "white" }}>{artist.name}</h3>
-              <img src={artist.image_url} alt="artist" />
-            </div>
-          </Cell>
-        </Grid>
-        <Border />
-        <div className="event" style={{ width: "50%", margin: "10vh auto" }}>
-          <h3>There are no upcoming events for the artist</h3>
-        </div>
-      </>
-    );
-  }
+  } 
+  return (
+    <>
+      <Grid style={mainStyles}>
+        <Cell col={12}>
+          <div id="title">
+            <h3 style={{ color: "white" }}>{artist.name}</h3>
+            <img src={artist.image_url} alt="artist" />
+          </div>
+        </Cell>
+      </Grid>
+      <Border />
+      <div className="event no-events">
+        <h3>There are no upcoming events for the artist</h3>
+      </div>
+    </>
+  );
 }
