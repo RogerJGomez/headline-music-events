2507b7d44595f3614ae91f82b2b68ddb5318969b
Roger Gómez <gomez.roger779@gmail.com>
2020-06-20 13:21:27 -0400

    Linting

--- src/App.js
+++ src/App.js
@@ -1,108 +1,129 @@
-import React, { useState, useEffect} from 'react'
-import axios from 'axios'
-import Artist from './components/Artist'
-import { Button, Textfield, Grid, Cell, Header, Navigation } from 'react-mdl'
-import { Lines } from 'react-preloaders'
-import LeftLogo from './components/LeftLogo'
-import RightLogo from './components/RightLogo'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Artist from "./components/Artist";
+import { Button, Textfield, Grid, Cell, Header, Navigation } from "react-mdl";
+import { Lines } from "react-preloaders";
+import LeftLogo from "./components/LeftLogo";
+import RightLogo from "./components/RightLogo";
 
-const iconStyle = {display:'flex', alignItems:'center', justifyContent:'center'}
+const iconStyle = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+};
 
 export default function App() {
-  const [artist, setArtist] = useState({})
-  const [events, setEvents] = useState([])
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(false)
-  const [preLoader, setPreLoader] = useState(false)
+  const [artist, setArtist] = useState({});
+  const [events, setEvents] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [preLoader, setPreLoader] = useState(false);
 
   useEffect(() => {
-    let event = "https://rest.bandsintown.com/artists/The%20Kooks/events?app_id=510"
-    let artist = "https://rest.bandsintown.com/artists/The%20Kooks?app_id=510"
-    const requestOne = axios.get(event)
-    const requestTwo = axios.get(artist)
+    let event =
+      "https://rest.bandsintown.com/artists/The%20Kooks/events?app_id=510";
+    let artist = "https://rest.bandsintown.com/artists/The%20Kooks?app_id=510";
+    const requestOne = axios.get(event);
+    const requestTwo = axios.get(artist);
 
-    axios.all([requestOne, requestTwo]).then(axios.spread((...responses) => {
-      const eventsData = responses[0].data
-      const artistData = responses[1].data  
-    
-      setArtist(artistData)
-      setEvents(eventsData)
-      setLoading(false)
-      setPreLoader(false)
-      
-    }))
-    .catch(errors => {
-      console.log(errors)
-      setLoading(false)
-      setPreLoader(false)
-      setError(true)
-    })
-  }, [])
+    axios
+      .all([requestOne, requestTwo])
+      .then(
+        axios.spread((...responses) => {
+          const eventsData = responses[0].data;
+          const artistData = responses[1].data;
 
+          setArtist(artistData);
+          setEvents(eventsData);
+          setLoading(false);
+          setPreLoader(false);
+        })
+      )
+      .catch((errors) => {
+        console.log(errors);
+        setLoading(false);
+        setPreLoader(false);
+        setError(true);
+      });
+  }, []);
 
-  function onSubmit (e){
-    e.preventDefault()
-      setError(false)
-      setLoading(true)
-      let artistName = e.target[0].value
-      let event = `https://rest.bandsintown.com/artists/${artistName}/events?app_id=510`
-      let artist = `https://rest.bandsintown.com/artists/${artistName}?app_id=510`
-      const requestOne = axios.get(event)
-      const requestTwo = axios.get(artist)
-  
-      axios.all([requestOne, requestTwo]).then(axios.spread((...responses) => { 
-        const eventsData = responses[0].data
-        const artistData = responses[1].data  
-      
-        setArtist(artistData)
-        setEvents(eventsData)
-        setLoading(false)
-      }))
-      .catch(errors => {
-        console.log(errors)
-        setLoading(false)
-        setError(true)
-      })
+  function onSubmit(e) {
+    e.preventDefault();
+    setError(false);
+    setLoading(true);
+    let artistName = e.target[0].value;
+    let event = `https://rest.bandsintown.com/artists/${artistName}/events?app_id=510`;
+    let artist = `https://rest.bandsintown.com/artists/${artistName}?app_id=510`;
+    const requestOne = axios.get(event);
+    const requestTwo = axios.get(artist);
 
-      e.target[0].value = ''
+    axios
+      .all([requestOne, requestTwo])
+      .then(
+        axios.spread((...responses) => {
+          const eventsData = responses[0].data;
+          const artistData = responses[1].data;
+
+          setArtist(artistData);
+          setEvents(eventsData);
+          setLoading(false);
+        })
+      )
+      .catch((errors) => {
+        console.log(errors);
+        setLoading(false);
+        setError(true);
+      });
+
+    e.target[0].value = "";
   }
 
   return (
     <>
-    <div className="App">
-      <Header title="Headline Music Events" style={{color: 'white', background:'#272727'}}>
+      <div className="App">
+        <Header
+          title="Headline Music Events"
+          style={{ color: "white", background: "#272727" }}
+        >
           <Navigation>
-              <a href="https://github.com/rogerjgomez" style={{lineHeight:0}}><i className="fa fa-github nav-icon"/></a>
+            <a href="https://github.com/rogerjgomez" style={{ lineHeight: 0 }}>
+              <i className="fa fa-github nav-icon" />
+            </a>
           </Navigation>
-      </Header>
-      <form onSubmit = {onSubmit}>
-        <Grid style={{background:'#7118d7', color:'white', paddingTop:'5vh'}}>
-          <Cell col={4} style={iconStyle}>
+        </Header>
+        <form onSubmit={onSubmit}>
+          <Grid
+            style={{ background: "#7118d7", color: "white", paddingTop: "5vh" }}
+          >
+            <Cell col={4} style={iconStyle}>
               <LeftLogo />
-          </Cell>
-          <Cell col={4} style={{textAlign:'center'}} id="search-form-">
-            <div className="flex-box">
-              <h3 id="search">Search an Artist</h3>
+            </Cell>
+            <Cell col={4} style={{ textAlign: "center" }} id="search-form-">
+              <div className="flex-box">
+                <h3 id="search">Search an Artist</h3>
                 <Textfield
-                    label="Search by name"
-                    style={{width: '400px'}}
-                    floatingLabel
+                  label="Search by name"
+                  style={{ width: "400px" }}
+                  floatingLabel
                 />
-                <Button style={{margin:'10px'}}raised colored>Search <i className="fa fa-search" /></Button>
-            </div>
-          </Cell>
-          <Cell col={4} style={iconStyle}>
+                <Button style={{ margin: "10px" }} raised colored>
+                  Search <i className="fa fa-search" />
+                </Button>
+              </div>
+            </Cell>
+            <Cell col={4} style={iconStyle}>
               <RightLogo />
-          </Cell>
-        </Grid>
-      </form>
-      <Artist events={events} artist={artist} error={error} loading={loading}/>
-    </div>
-    <Lines customLoading={preLoader} background="#00af9a" color={'#7118d7'}/>
-  </>  
-  )
+            </Cell>
+          </Grid>
+        </form>
+        <Artist
+          events={events}
+          artist={artist}
+          error={error}
+          loading={loading}
+        />
+      </div>
+      <Lines customLoading={preLoader} background="#00af9a" color={"#7118d7"} />
+    </>
+  );
 }
-
-
-
-
--- src/components/Event.js
+++ src/components/Event.js
@@ -1,24 +1,32 @@
-import React from 'react'
-import { Cell } from 'react-mdl'
-import { Fade } from 'react-reveal'
+import React from "react";
+import { Cell } from "react-mdl";
+import { Fade } from "react-reveal";
 
-export default function Event({event}) {
-       const FormatDate = () => {
-          const date = new Date(event.datetime).toDateString()
-          return(date)
-       }
-        return ( 
-            <Cell col={4}>
-                <Fade right>
-                    <div className="event">
-                        <h3>{event.venue.name}</h3>
-                        <h4>{event.venue.city} - {event.venue.country}</h4>
-                        <h4>{FormatDate()}</h4>
-                        <a href={event.url} target="_blank" rel="noopener noreferrer" className="mdl-button mdl-js-button mdl-button mdl-button--raised mdl-button--colored">Get 
-                            Tickets <i className="fa fa-ticket" style={{color: '#F3B904'}}/>
-                        </a>
-                    </div>
-                </Fade>
-            </Cell>
-        )
+export default function Event({ event }) {
+  const FormatDate = () => {
+    const date = new Date(event.datetime).toDateString();
+    return date;
+  };
+  return (
+    <Cell col={4}>
+      <Fade right>
+        <div className="event">
+          <h3>{event.venue.name}</h3>
+          <h4>
+            {event.venue.city} - {event.venue.country}
+          </h4>
+          <h4>{FormatDate()}</h4>
+          <a
+            href={event.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mdl-button mdl-js-button mdl-button mdl-button--raised mdl-button--colored"
+          >
+            Get Tickets{" "}
+            <i className="fa fa-ticket" style={{ color: "#F3B904" }} />
+          </a>
+        </div>
+      </Fade>
+    </Cell>
+  );
 }
